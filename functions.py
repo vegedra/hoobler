db7b1d3de85a348bf601b2b9425e71453e77e19b
vegedra <vegedracp@gmail.com>
2024-08-20 17:45:22 -0300

    exportado de forma automatica em 20-08-2024 17:45

--- functions.py
+++ functions.py
@@ -1,19 +1,22 @@
 import random
 import requests
 
-# Function to Generate weather Report
+# Receber informações do clima.
 def get_weather():
-    city_name = input("Enter the name of the city you want the weather report for: ")
+    city_name = input("\nEnter the name of the city you want the weather report for: ")
 
-    # Generate report directly within get_weather
-    url = 'https://wttr.in/{}'.format(city_name)
+    # Usa o sistema WTTR para ver o clima - ?T - Remove cor e faz arte ASCII funcionar e o 0 para mostrar apenas
+    # o clima atual
+    url = 'https://wttr.in/{}?T&0'.format(city_name)
     try:
         data = requests.get(url)
         T = data.text
     except Exception as e:
         T = f"Error occurred: {e}"
-    print(T)
+    # Printa o clima e a arte ASCII
+    return T
 
+# Conta piadas
 def tell_joke():
     jokes = [
         "Why don't scientists trust atoms? Because they make up everything!",
